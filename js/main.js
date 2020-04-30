3c8f78d4208daef38bd6e034e12e0bc4aadf7e0f
vladimirpotanin <vladimir971001@yandex.ru>
2020-04-30 19:27:06 +0200

    'Added'

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 let selectedIndex = 0;
 let coinsValue = [1, 5, 10, 25, 50, 100];
+let small = [0, 1, 2, 3];
+let big = [6, 7, 8, 9];
+let even = [1, 3, 7, 9];
+let odd = [0, 2, 6, 8];
 $(document).ready(function() {
-
     $('.coins img').click(function() {
         $('.coins img').removeClass('active');
         $(this).addClass('active');
@@ -17,4 +20,38 @@ $(document).ready(function() {
     $('.range-container , .number-box').click(function() {
         $(this).prepend("<img id='coin' src='img/coin-" + coinsValue[selectedIndex] + ".svg' />")
     });
+    $(".range-container").eq(0).mouseover(function() {
+        $('.number-box').removeClass('active-box');
+        let numberBox = $('.number-box');
+        for (let i = 0; i < small.length; i++) {
+            numberBox.eq(small[i]).addClass('active-box');
+        }
+    });
+    $(".range-container").eq(1).mouseover(function() {
+        $('.number-box').removeClass('active-box');
+        let numberBox = $('.number-box');
+        for (let i = 0; i < small.length; i++) {
+            numberBox.eq(odd[i]).addClass('active-box');
+        }
+    });
+    $(".range-container").eq(2).mouseover(function() {
+        $('.number-box').removeClass('active-box');
+        let numberBox = $('.number-box');
+        for (let i = 0; i < small.length; i++) {
+            numberBox.eq(big[i]).addClass('active-box');
+        }
+    });
+    $(".range-container").eq(3).mouseover(function() {
+        $('.number-box').removeClass('active-box');
+        let numberBox = $('.number-box');
+        for (let i = 0; i < small.length; i++) {
+            numberBox.eq(even[i]).addClass('active-box');
+        }
+    });
+    $(".number-box").mouseover(function() {
+        $('.number-box').removeClass('active-box');
+        let numberBox = $('.number-box');
+        $(this).addClass('active-box');
+    });
+
 })
